f2aaaad354aaf29cd3884b72d4c25fe77565785b
Jahmai OSullivan <jaosulli@microsoft.com>
2016-04-29 17:53:13 -0400

    constructing post

--- app/containers/About.jsx
+++ app/containers/About.jsx
@@ -9,7 +9,7 @@ import styles from 'css/components/about';
 const About = () => {
   return (
     <div className={styles.about}>
-      <h1 className={styles.header}>Wow what</h1>
+      <h1 className={styles.header}>Wow waa</h1>
       <div className={styles.description}>
         <p>Imagine an ocean of ninjas. Now think of it as a metaphor.<br />
           Seriously, we love good tech. React, redux, scala, Haskell, machine learning, you name it!
